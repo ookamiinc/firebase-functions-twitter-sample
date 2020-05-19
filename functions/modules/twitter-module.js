c2006217ffd5553c89d5bdffe2303b24cac216f7
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-05-19 20:03:29 +0900

    Add search method in TwitterModule

--- functions/modules/twitter-module.js
+++ functions/modules/twitter-module.js
@@ -16,6 +16,53 @@ class TwitterModule {
     }
 
     /**
+     * 対象 Twitter Search 結果のツイートリスト取得
+     * @link https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/get-favorites-list
+     * @param {Object} options - ツイート取得オプション
+     * @param {callback} callback
+     * @example
+     * options = {
+     *  q: string - 検索クエリ
+     *  count: int - ツイート取得数
+     *  since_id: int - 指定されたツイート ID 以降のツイートを取得
+     * }
+     */
+    search(options, callback) {
+        const cacheLifeTimeSec = 60 // 1 min
+        const apiPath = 'search/tweets'
+        const tweets = this.cache.get(apiPath)
+        if (tweets) {
+            return callback(tweets, false)
+        } else {
+            const self = this;
+            let requestParams = {}
+            if ('q' in options) {
+                requestParams.q = options.q
+            }
+            if ('count_limit' in options) {
+                requestParams.count = options.count_limit
+            }
+            if ('count' in options && 'count_limit' in options) {
+                if (options.count_limit > options.count) {
+                    requestParams.count = options.count
+                } else {
+                    requestParams.count = options.count_limit
+                }
+            }
+            if ('since_id' in options) {
+                requestParams.since_id = options.since_id
+            }
+
+            this.client.get(apiPath, requestParams, (error, tweets, response) => {
+                if (tweets) {
+                    self.cache.put(apiPath, tweets, cacheLifeTimeSec * 1000)
+                }
+                return callback(tweets, error)
+            })
+        }
+    }
+
+    /**
      * 対象 Twitter ユーザーの いいね したツイートリスト取得
      * @link https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/get-favorites-list
      * @param {Object} options - ツイート取得オプション
